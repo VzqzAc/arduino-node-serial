d1eac14e03549ac380bb922a5322287eeb360381
Omar Vazquez <omar.vazquezac@gmail.com>
2016-10-31 10:06:44 -0600

    serial communication properly working

--- arduino-sketch/test/test.ino
+++ arduino-sketch/test/test.ino
@@ -4,17 +4,19 @@ String a;
 void setup() {
   Serial.begin(9600);
   a = "";
-  digitalWrite(13, LOW);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
   led = false;
 }
 
 void loop() {
   a = Serial.readString();
-  Serial.println(a);
   if(a.equals("led")) {
-    digitalWrite(13, toggleLed());
+    digitalWrite(LED_BUILTIN, toggleLed());
+    Serial.println(a);
+    delay(1500);
   }
-  delay(2);
+
 }
 
 boolean toggleLed() {
